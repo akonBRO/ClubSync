ce123fe1ffbf4645798e356153216eb6ce413e22
MD. SABBIR AKON <109897313+akonBRO@users.noreply.github.com>
2025-04-20 03:17:48 +0600

    .

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,1 @@
-const express= require('express')
-const mongoose = require('mongoose')
-const app = express()
-
-mongoose.connect('mongodb+srv://admin:admin@clubsync-db.04whpr6.mongodb.net/clubsync');
-const userSchema= new mongoose.Schema({
-    name:String,
-    age:Number
-})
-const userModel= mongoose.model("emp", userSchema);
-app.listen('3000', ()=>{
-    console.log('running!!!');
-})
+const express= require('express');
